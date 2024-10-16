a95cd842a1b52a98d58612ec6cbf9bfd0028d36d
Tomiwa <codertomiwa@gmail.com>
2024-10-16 22:40:02 +0100

    setting up categories on home page

--- AgroFresh/urls.py
+++ AgroFresh/urls.py
@@ -2,6 +2,8 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from core.views import index, contact
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
