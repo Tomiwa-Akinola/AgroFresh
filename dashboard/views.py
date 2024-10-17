00d7bdad526abe2aa93139d1fb7d570015e49375
Tomiwa <codertomiwa@gmail.com>
2024-10-17 15:21:59 +0100

    dashboard for sellers

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from item.models import item
+
+@login_required
+def index(request):
+    items = Item.objects.filter(created_by=request.user)
+
+    return render(request, 'dashboard/index.html', {
+        'items': items,
+        })
