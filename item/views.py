1b5d1c09898666d91ffb016f22b619eae49e0d56
Tomiwa <codertomiwa@gmail.com>
2024-10-17 11:16:44 +0100

    functionalilty for new items

--- item/views.py
+++ item/views.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 
 from .models import Item
@@ -11,4 +12,12 @@ def detail(request, pk):
     return render(request, 'item/detail.html', {
         'item': item,
         'related_items': related_items
+        })
+
+@login_required
+def new(request):
+    form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form
         })
