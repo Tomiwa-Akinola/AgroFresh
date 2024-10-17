db09a1207ce54ac5dfd9786cecd1a5925613c065
Tomiwa <codertomiwa@gmail.com>
2024-10-17 15:40:00 +0100

    fixed syntax error

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from item.models import item
+from item.models import Item
 
 @login_required
 def index(request):
