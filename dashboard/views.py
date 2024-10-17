bdd3d35baaa068ab7052a1afdfb2a4cf8b33d861
Tomiwa <codertomiwa@gmail.com>
2024-10-17 16:49:30 +0100

    functionality for deleting items

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from item.models import Item
+from item.models import item
 
 @login_required
 def index(request):
