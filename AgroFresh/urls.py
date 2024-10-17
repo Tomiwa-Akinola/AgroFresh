d63b2fa5333772f986ff43e23afde9f91b5b0af1
Tomiwa <codertomiwa@gmail.com>
2024-10-17 06:43:06 +0100

    linked the detail page to the home page

--- AgroFresh/urls.py
+++ AgroFresh/urls.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,6 +9,7 @@ from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
