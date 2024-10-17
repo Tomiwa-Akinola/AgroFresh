32784181203b08810c1d9f8d1114d49bd2f9ddc0
Tomiwa <codertomiwa@gmail.com>
2024-10-17 21:21:40 +0100

    search functionality

--- item/views.py
+++ item/views.py
@@ -9,6 +9,7 @@ from .models import Item, Category
 
 def items(request):
     query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
     categories = Category.objects.all()
     items = Item.objects.filter(is_sold=False)
 
@@ -19,6 +20,7 @@ def items(request):
         'items': items,
         'query': query,
         'categories': categories,
+        'category_id': int(category_id)
         })
 
 def detail(request, pk):
